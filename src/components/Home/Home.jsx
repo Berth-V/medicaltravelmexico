226e4a8aee34c9bb9aeac45568c6fab82e1ac5a7
Berth-V <menos_45@hotmail.com>
2023-08-23 17:43:08 -0700

    First animations

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,16 +10,41 @@ import travel8 from '/travel8.jpg';
 import {motion} from 'framer-motion';
 
 function Home() {
+
+  const frontPageAnimations={
+    start: {y:-100,opacity:0},
+    end: {y:0,opacity:1,
+      transition:{
+        staggerChildren:0.3,
+        duration:0.6
+      }
+    }
+  }
+
+  const proceduresCardsAnimation={
+    start: {y:-100,opacity:0},
+    end: {y:0, opacity:1, rotateY: [0, 360, 0]
+    }
+  }
+
   return (
       <div className="home">
         <section className='home__section1'
-          style={{background:`url(${portada}) no-repeat center/105%`}}
+          style={{background:`url(${portada}) no-repeat left/90% #FCFCFC`}}
         >
-          <motion.div className='home__textBox'>
-            <motion.h1 className='home__h1'>
+          <motion.div className='home__textBox'
+            variants={frontPageAnimations}
+            initial='start'
+            whileInView='end'
+          >
+            <motion.h1 className='home__h1'
+              variants={frontPageAnimations}
+            >
               Lorem ipsum dolor sit amet consectetur.
             </motion.h1>
-            <motion.p className='home__p'>
+            <motion.p className='home__p'
+              variants={frontPageAnimations}
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Atque doloribus quam consequatur beatae voluptas id voluptatibus 
               ratione magni culpa omnis, deleniti recusandae ut ullam, maiores 
@@ -29,8 +54,14 @@ function Home() {
           </motion.div>
         </section>
         <section  className='home__section2'>
-          <div className='box'>
-            <div className='item'>
+          <motion.div className='box'>
+            <motion.div className='item'
+              variants={proceduresCardsAnimation}
+              initial='start'
+              whileInView='end'
+              viewport={{amount:1,once:true}}
+              transition={{duration:0.5,delay:0}}
+            >
               <img className='box__icon'
                 src={bariatric}
               />
@@ -39,8 +70,14 @@ function Home() {
               Doloremque ipsam aspernatur veritatis modi commodi, quia 
               facere temporibus voluptatem dolorum maxime? Et enim harum 
               nihil saepe rem esse hic. Architecto, laborum.</p>
-            </div>
-            <div className='item'>
+            </motion.div>
+            <motion.div className='item'
+              variants={proceduresCardsAnimation}
+              initial='start'
+              whileInView='end'
+              viewport={{amount:1,once:true}}
+              transition={{duration:0.5,delay:0.2}}
+            >
               <img className='box__icon' 
                 src={trauma}
               />
@@ -49,8 +86,14 @@ function Home() {
               Doloremque ipsam aspernatur veritatis modi commodi, quia 
               facere temporibus voluptatem dolorum maxime? Et enim harum 
               nihil saepe rem esse hic. Architecto, laborum.</p>
-            </div>
-            <div className='item'>
+            </motion.div>
+            <motion.div className='item'
+              variants={proceduresCardsAnimation}
+              initial='start'
+              whileInView='end'
+              viewport={{amount:1,once:true}}
+              transition={{duration:0.5,delay:0.4}}
+            >
               <img className='box__icon' 
                 src={plastic}
               />
@@ -59,8 +102,14 @@ function Home() {
               Doloremque ipsam aspernatur veritatis modi commodi, quia 
               facere temporibus voluptatem dolorum maxime? Et enim harum 
               nihil saepe rem esse hic. Architecto, laborum.</p>
-            </div>
-            <div className='item'>
+            </motion.div>
+            <motion.div className='item'
+              variants={proceduresCardsAnimation}
+              initial='start'
+              whileInView='end'
+              viewport={{amount:1,once:true}}
+              transition={{duration:0.5,delay:0.6}}
+            >
               <img className='box__icon' 
                 src={oto}
               />
@@ -69,11 +118,15 @@ function Home() {
               Doloremque ipsam aspernatur veritatis modi commodi, quia 
               facere temporibus voluptatem dolorum maxime? Et enim harum 
               nihil saepe rem esse hic. Architecto, laborum.</p>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </section>
         <section  className='home__section3'>
-          <img className='airplane'
+          <motion.img className='airplane'
+            initial={{x:-1700}}
+            whileInView={{x:0}}
+            transition={{duration:1,delay:0.5}}
+            viewport={{once:true}}
             src={airplane}
             alt='travel airplane'
           />
